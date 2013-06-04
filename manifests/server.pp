d544e2db2eb18826c677ee45aad28abd6698ace7
Michael Crumm <mike.crumm@groundctrl.com>
2013-06-03 21:12:16 -0700

    Adding gis repo and postgis packages

--- manifests/server.pp
+++ manifests/server.pp
@@ -5,3 +5,27 @@ apache::vhost { "${fqdn}":
   docroot => "${fqdn}"
 }
 
+class { 'apt':
+  always_apt_update    => false,
+  disable_keys         => false,
+  purge_sources_list   => false,
+  purge_sources_list_d => false,
+  purge_preferences_d  => false
+}
+
+apt::source { 'ubuntugis':
+  location   => 'http://ppa.launchpad.net/ubuntugis',
+  repos      => 'main',
+  key        => '314DF160',
+  key_server => 'keyserver.ubuntu.com',
+}
+
+package { 'postgresql-9.1-postgis'
+  ensure => present
+}
+
+package { 'postgresql-contrib-9.1'
+  ensure => present
+}
+
+
